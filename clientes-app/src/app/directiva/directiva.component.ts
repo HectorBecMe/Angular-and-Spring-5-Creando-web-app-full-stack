85204daed88cf182b7ff78bfc5aac83ee573e4d3
HectorBecMe <hectorbecme@gmail.com>
2020-08-21 17:25:37 -0500

    seccion02 - 014
    Directiva structural *ngFor

---
+++ clientes-app/src/app/directiva/directiva.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html'
+})
+export class DirectivaComponent {
+
+  listaCurso: string[] = ['TypeScript', 'JavaScript', 'PHP', 'Java SE', 'C#'];
+  constructor() { }
+
+}
