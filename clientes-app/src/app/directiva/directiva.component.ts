f4e720c7b6d621a2e7c267ae7032b8a6915147da
HectorBecMe <hectorbecme@gmail.com>
2020-08-24 02:36:42 -0500

    seccion02 - 015
    Directiva estructural *ngif

--- clientes-app/src/app/directiva/directiva.component.ts
+++ clientes-app/src/app/directiva/directiva.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 export class DirectivaComponent {
 
   listaCurso: string[] = ['TypeScript', 'JavaScript', 'PHP', 'Java SE', 'C#'];
+
+  tituloBoton = 'Ocultar';
+  habilitar = true;
+
   constructor() { }
+
+  cambiar(): void {
+    this.habilitar = !this.habilitar;
+    this.tituloBoton = this.habilitar ? 'Ocultar' : 'Mostrar';
+  }
 
 }
