cf943af9d0aa91e1caf32646dfd9c26a901daa1e
HectorBecMe <hectorbecme@gmail.com>
2020-08-25 06:57:00 -0500

    seccion03 - 017
    Listando los objetos del tipo Cliente

--- clientes-app/src/app/clientes/clientes.component.ts
+++ clientes-app/src/app/clientes/clientes.component.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
 
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html'
 })
 export class ClientesComponent implements OnInit {
+
+  clientes: Cliente[] =[
+    {id: 3, nombre: 'Hector', apellido: 'BecMe', email: 'yo@hectorbecme.com', createAt: '2020,08,25'},
+    {id: 4, nombre: 'Hector4', apellido: 'BecMe', email: 'yo@hectorbecme.com', createAt: '2020,08,25'},
+    {id: 5, nombre: 'Hector5', apellido: 'BecMe', email: 'yo@hectorbecme.com', createAt: '2020,08,25'},
+    {id: 6, nombre: 'Hector6', apellido: 'BecMe', email: 'yo@hectorbecme.com', createAt: '2020,08,25'}
+  ];
 
   constructor() { }
 
